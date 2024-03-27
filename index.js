0f4d313ce941c8d400b309923d26e4f44c9cf637
scarlet <sarahscarlet641@gmail.com>
2024-03-27 17:26:13 +0300

    create array.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// code your solution here
+function superbowlWin(record) {
+    const win = record.find(entry => entry.team === 'Denver Broncos');
+      console.log("Denver Broncos")  
+}
+function superbowlWin(record) {
+    const winRecord  = record.find(element=> element.result === "W");
+    return winRecord ? winRecord.year: undefined; 
+      console.log(superbowlWin(record));
+}
